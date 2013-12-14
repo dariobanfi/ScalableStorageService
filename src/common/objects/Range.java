cd6de7f713c375c74b2e13410602ea2dcf701f20
Ambigioz <ambigioz@gmail.com>
2013-12-14 19:31:14 +0100

    Almost every object is serializable bitch!

--- src/common/objects/Range.java
+++ src/common/objects/Range.java
@@ -1,12 +1,54 @@
 package common.objects;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Range {
 	
 	private String lower_limit;
 	private String upper_limit;
+	private static final byte COLUMN = 58;
 	/**
 	 * @return the lower_limit
 	 */
+	
+	public Range(String lower_limit, String upper_limit){
+		this.lower_limit = lower_limit;
+		this.upper_limit = upper_limit;
+	}
+	
+	public Range(byte[] bytes){
+		int i=0;
+		List<Byte> readlowerlimit =  new ArrayList<Byte>();
+        List<Byte> readupperlimit = new ArrayList<Byte>();
+        
+        // Reading server address
+		while(i<bytes.length){
+            if(bytes[i]==COLUMN){
+                i++;
+                break;
+            }
+            readlowerlimit.add(bytes[i]);
+            i++;
+		}
+		byte[] tmpreadlowerlimit = new byte[readlowerlimit.size()];
+        for (int j = 0; j < readlowerlimit.size(); j++){
+        	tmpreadlowerlimit[j] = readlowerlimit.get(j);
+        }
+        this.lower_limit = new String(tmpreadlowerlimit);
+		
+        //Reading port address
+		while(i<bytes.length){
+			readupperlimit.add(bytes[i]);
+			i++;
+		}
+		byte[] tmpreadupperlimit = new byte[readupperlimit.size()];
+        for (int j = 0; j < readupperlimit.size(); j++){
+        	tmpreadupperlimit[j] = readupperlimit.get(j);
+        }
+        this.upper_limit = new String(tmpreadupperlimit);	
+	}
+	
 	public String getLower_limit() {
 		return lower_limit;
 	}
@@ -29,5 +71,9 @@ public class Range {
 		this.upper_limit = upper_limit;
 	}
 	
+	public byte[] getBytes(){
+		return (lower_limit + ":" + upper_limit).getBytes();
+	}
 	
+
 }
